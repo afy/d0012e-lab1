c6a5eae315e97528b62d4f11d124e27d3f5b2330
afy <hannes.furhoff@hotmail.com>
2021-11-09 12:35:38 +0100

    fixed some stuff

--- implementation/bSort.py
+++ implementation/bSort.py
@@ -15,10 +15,9 @@ def binarySearch(e, a, l, h):
     else: # stega upp
         return binarySearch(e, a, m + 1, h)
 
-
 def binarySort(a):
     na = [a[0]]
-    for i in range(1, len(a)-1):
+    for i in range(1, len(a)):
         e = a[i]
         ni = binarySearch(e, na, 0, len(na)-1)
         na = na[:ni] + [e] + na[ni:]
@@ -42,23 +41,25 @@ def mergeSort(a, sort):
     r = sort(mergeSort(a[:mid], sort))    
     return sort(l + r)
         
-
-l = [rnd.randint(0, 99) for x in range(0,10000)]
-print("=")
-s1 = time.time()
-r1 = mergeSort(l, insertionSort)
-e1 = time.time()
-print("==")
-s2 = time.time()
-r2 = insertionSort(l)
-e2 = time.time()
-print("===")
-s3 = time.time()
-r3 = mergeSort(l, binarySort)
-e3 = time.time()
-print("====")
-s4 = time.time()
-r4 = binarySort(l)
-e4 = time.time()
-print("M(I): {}  I: {}  M(B): {}  B: {}"
-      .format(e1-s1, e2-s2, e3-s3, e4-s4))
+##
+##l = [rnd.randint(0, 99) for x in range(0,10)]
+##print(l)
+##print("=")
+##s1 = time.time()
+##r1 = mergeSort(l, insertionSort)
+##e1 = time.time()
+##print("==")
+##s2 = time.time()
+##r2 = insertionSort(l)
+##e2 = time.time()
+##print("===")
+##s3 = time.time()
+##r3 = mergeSort(l, binarySort)
+##e3 = time.time()
+##print("====")
+##s4 = time.time()
+##r4 = binarySort(l)
+##e4 = time.time()
+##print("{}: {}".format(e3-s3, r3))
+##print("M(I): {}  I: {}  M(B): {}  B: {}"
+##      .format(e1-s1, e2-s2, e3-s3, e4-s4))
--- implementation/main.py
+++ implementation/main.py
@@ -4,12 +4,13 @@ import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 import random as rnd
-import threading
+import time
+from bSort import *
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 matplotlib.use("TkAgg")
 fig, axs = plt.subplots(1, 1)
 
-nmax = 10000
+nmax = 15000
 kmax = 1
 
 timeIS = [0 for x in range(0, nmax)]
@@ -21,53 +22,13 @@ def insertionSortExec(l):
     return [len(l), len(l)-1]
 
 def mergeSortExec(l, k):
-    return [k, l[k]]
-
-class MyThread (threading.Thread):
-   def __init__(self, l, o, t):
-      threading.Thread.__init__(self)
-      self.l = l
-      self.o = o
-      self.t = t 
-   def run(self):
-        for i in range(0, len(self.l)):
-            IS = insertionSortExec(self.l)
-            timeIS[i+self.o] = IS[0]
-            opsIS[i+self.o] = IS[1]
-
-            MS = mergeSortExec(self.l, 1)
-            timeMS[i+self.o] = MS[0]
-            opsMS[i+self.o] = MS[1]
-        self.t[self.o-1] = 1
-
-        show = True
-        for el in self.t:
-            if el != 1:
-                show = False
-        if show:
-            axs.plot(timeIS, label="IS time")
-            axs.plot(opsIS, label="IS ops")
-            axs.plot(timeMS, label="MS time")
-            axs.plot(opsMS, label="MS ops")
-            axs.set_xlim(0, nmax)
-            axs.set_ylim(0, 200)
-            axs.set_xlabel('size')
-            axs.set_ylabel('sort')               
-
-
-def test1_t():
-    l1 = [x for x in range(nmax)]
-    t = []
-    print("finised calculating l1")
-    for i in range(0, nmax//1000):
-        MyThread(l1[0:i], i, t).start()
-        t.append(0)
+    return [k, l[k]]         
 
 def test1(): # timeIS opsIS timeMS opsMS
     l1 = [x for x in range(nmax)]
     print("finised calculating l1")
     for n in range(0, nmax):
-        i = l1[0:nmax]
+        i = l1[0:n]
         rnd.shuffle(i)
         IS = insertionSortExec(i)
         timeIS.append(IS[0])
@@ -84,11 +45,57 @@ def test1(): # timeIS opsIS timeMS opsMS
 
 
 def test2(): # timeMS opsMS n/k
-    pass
+    l2 = [x for x in range(nmax)]
+    rnd.shuffle(l2)
+    timeMIS = []
+    timeMBS = []
+    timeIS = []
+    nscale = []
+    for n in range(1, nmax, nmax//20):
+        i = l2[0:n+1]
+        nscale.append(n)
+        ri = i.copy()
+
+        si = time.time()
+        ri.sort()
+        ei = time.time()
+        timeIS.append(ei-si)
+        
+        s1 = time.time()
+        r1 = mergeSort(i, insertionSort)
+        e1 = time.time()
+        timeMIS.append(e1-s1)
+
+        s2 = time.time()
+        r2 = mergeSort(i, binarySort)
+        e2 = time.time()
+        timeMBS.append(e2-s2)
+
+##        print(ri)
+##        print(r1)
+##        print(r2)
+        
+        if r1 != ri:
+            print("wrong value for 1")
+        if r2 != ri:
+            print("wrong value for 2")
+            
+        print("{}/{}".format(n, nmax))
+
+        
+    axs.plot(nscale, timeMIS,  label="MIS time")
+    axs.plot(nscale, timeMBS, label="MBS time")
+    axs.plot(nscale, timeIS, label="Inbuilt")
+    ##.set_xlim(0, nmax)
+    ##axs.set_ylim(0, 200)
+    axs.set_xlabel('sample size')
+    axs.set_ylabel('time')               
+
+        
     
 
 
-test1()
+test2()
 axs.grid(True)
 plt.legend(loc='best')
 fig.tight_layout()
